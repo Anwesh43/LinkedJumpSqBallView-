3f7f866b17eac19e013667c7cadfff4397b5bbce
Anwesh43 <anweshthecool0@gmail.com>
2020-05-16 08:10:12 +0530

    adding constants and libraries

--- jumpsqballview/src/main/java/com/anwesh/uiprojects/jumpsqballview/JumpSqBallView.kt
+++ jumpsqballview/src/main/java/com/anwesh/uiprojects/jumpsqballview/JumpSqBallView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.jumpsqballview
 /**
  * Created by anweshmishra on 16/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.02f
+val rFactor : Float = 3.2f
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
