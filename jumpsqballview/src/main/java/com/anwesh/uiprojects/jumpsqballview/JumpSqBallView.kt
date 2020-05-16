be5823d7bd6ea82aeaaeeff456b6c166c3d24627
Anwesh43 <anweshthecool0@gmail.com>
2020-05-16 08:20:42 +0530

    adding JumpSqBallView class

--- jumpsqballview/src/main/java/com/anwesh/uiprojects/jumpsqballview/JumpSqBallView.kt
+++ jumpsqballview/src/main/java/com/anwesh/uiprojects/jumpsqballview/JumpSqBallView.kt
@@ -50,3 +50,19 @@ fun Canvas.drawRSBNode(i : Int, scale : Float, paint : Paint) {
     drawRotatingSqBall(scale, size, h, paint)
     restore()
 }
+
+class JumpSqBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
