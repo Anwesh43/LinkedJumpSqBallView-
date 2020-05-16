82a52a257b41fddac93ab2d6d10cef3a220bd727
Anwesh43 <anweshthecool0@gmail.com>
2020-05-16 08:24:20 +0530

    adding Animator class

--- jumpsqballview/src/main/java/com/anwesh/uiprojects/jumpsqballview/JumpSqBallView.kt
+++ jumpsqballview/src/main/java/com/anwesh/uiprojects/jumpsqballview/JumpSqBallView.kt
@@ -19,6 +19,7 @@ val foreColor : Int = Color.parseColor("#673AB7")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rotDeg : Float = 180f
 val sqHFactor : Float = 4f
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -82,6 +83,34 @@ class JumpSqBallView(ctx : Context) : View(ctx) {
             if (dir == 0f) {
                 dir = 1f - 2 * prevScale
                 cb()
+            }
+        }
+    }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(delay)
+                    view.invalidate()
+                } catch(ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
             }
         }
     }
